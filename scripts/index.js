27730cfd703211c29550288068f5b309adb9a564
Deepak Ahire <adeepak7@Deepaks-MacBook-Pro.local>
2020-08-26 18:16:34 +0530

    First workign template

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+$(function(){
+
+    let hash = location.hash;
+    if(sessionStorage){
+        var target = sessionStorage.getItem("target");
+        if(target==undefined || target==null || target=="") {
+            sessionStorage.setItem("target", hash.length > 0 ? hash.substr(1) : "home");
+        }
+    }
+
+    function getContent(target){
+
+        $.get(target + ".html").then(
+            function(response){
+                $("#content-wrapper").html(response);
+            }
+        );
+    }
+
+    getContent(sessionStorage.getItem("target"));
+
+
+    $(".navview-menu").on(Metro.events.click, "a", function(e){
+
+        var href = $(this).attr("href");
+        var hash;
+
+        console.log("HREF: ", href);
+
+
+        if (href === "#") {
+            return false;
+        }
+
+        if (href === "/") {
+            return false;
+        }
+
+        hash = href.substr(1);
+        href = hash + ".html";
+
+        getContent(hash);
+
+        sessionStorage.setItem("target", hash);
+
+        return false;
+    });
+
+});
